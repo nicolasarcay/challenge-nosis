3f8c55f451ff234347acf51e11f86c258d15babe
nicolasarcay <hola@nicolasarcay.com>
2022-09-04 23:03:26 -0300

    github pages routers

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pet-adoption-app">
+    <BrowserRouter basename="/challenge-nosis">
       <MoviesApp />
     </BrowserRouter>
   </React.StrictMode>
